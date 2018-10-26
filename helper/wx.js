1a0ee8f53fc47702d22614bb0dab79ad899f8d6a
San <464884785@qq.com>
2018-10-26 16:52:31 +0800

    '替换html2wxml'

--- helper/wx.js
+++ helper/wx.js
@@ -9,7 +9,7 @@ export const store = {
     return app().data[key]
   },
 }
-export const setState = function (data) {
+export const setState = function(data) {
   this.setData(data)
   return Promise.resolve(data)
 }
@@ -19,7 +19,7 @@ export class Audio {
     this.canPlay = () => null
     this.ctx = wx.createInnerAudioContext()
     this.ctx.autoplay = false
-    this.ctx.obeyMuteSwitch	= false
+    this.ctx.obeyMuteSwitch = false
     this.ctx.src = encodeURI(src)
     this.ctx.onTimeUpdate(() => this.playCall(this.ctx.currentTime, this.ctx.duration))
     this.ctx.onPlay(() => null)
@@ -51,4 +51,33 @@ export class Audio {
   destroy() {
     this.ctx.destroy()
   }
+}
+
+export const helper = {
+  clearText: function (text) {
+    const audio = /<audio[^>].*>[\s\S]+<\/audio>/g
+    const img = /<img.*?src="http:\/\/guo\.lu\/wp-content\/uploads\/2014\/downloading\.png".*?\/>/g
+    const script = /<!--\[if lt IE 9]>.*?<!\[endif]-->/g
+    const entity = /&.*?;/g
+    const br = /<br \/>/g
+
+    return text
+      .replace(audio, '')
+      .replace(img, '')
+      .replace(script, '')
+      .replace(entity, '')
+      .replace(br, '<p>')
+
+  },
+  wordTotal: function (text){
+    const per = 400
+    let total = 0
+    for (let i = 0; i < text.length; i += 1) {
+      if (text.charCodeAt(i) > 127 || text.charCodeAt(i) === 94) {
+        total += 1
+      }
+    }
+    return { total, time: Math.floor(total / per) }
+  },
+
 }
--- pages/post/index.js
+++ pages/post/index.js
@@ -1,32 +1,7 @@
-import { store, setState, Audio } from '../../helper/wx'
+import { store, setState, Audio, helper } from '../../helper/wx'
 import request from '../../helper/request'
-import { html2json } from '../../helper/html2wxml/html2json.js'
+import WxParse from '../../helper/wxParse/wxParse.js'
 
-const clearText = (text) => {
-  const audio = /<audio[^>].*>[\s\S]+<\/audio>/g
-  const img = /<img.*?src="http:\/\/guo\.lu\/wp-content\/uploads\/2014\/downloading\.png".*?\/>/g
-  const script = /<!--\[if lt IE 9]>.*?<!\[endif]-->/g
-  const entity = /&.*?;/g
-  const br = /<br \/>/g
-
-  return text
-    .replace(audio, '')
-    .replace(img, '')
-    .replace(script, '')
-    .replace(entity, '')
-    .replace(br, '<p>')
-}
-
-const word = (text) => {
-  const per = 400
-  let total = 0
-  for (let i=0; i < text.length; i += 1) {
-    if (text.charCodeAt(i) > 127 || text.charCodeAt(i) === 94) {
-      total += 1
-    }
-  }
-  return { total, time: Math.floor(total / per) }
-}
 
 Page({
   data: {
@@ -38,6 +13,7 @@ Page({
   },
 
   setState,
+  helper,
 
   audio: null,
 
@@ -86,8 +62,10 @@ Page({
   },
 
   onLoad({ id }) {
+    var that = this;
     const posts = store.get('post')
 
+    //是否已缓存
     if (posts[id]) {
       this.setState({ post: posts[id], innerHTML: posts[id].innerHTML })
       this.initAudio()
@@ -98,13 +76,14 @@ Page({
       .then(({ data: post }) => {
         const { rendered } = post.content
 
-        post.content.rendered = clearText(rendered)
-        post.content = { ...word(rendered), ...post.content }
+        post.content.rendered = that.helper.clearText(rendered)
+        post.content = { ...that.helper.wordTotal(rendered), ...post.content }
         post.date = post.date.split('T')[0]
-        post.innerHTML = html2json(post.content.rendered).child
+      
+        WxParse.wxParse('innerHTML', 'html', post.content.rendered, that, 5);
+        post.innerHTML = that.data.innerHTML;
 
-
-        return this.setState({ post, innerHTML: post.innerHTML })
+        return this.setState({post})
       })
       .then(({ post }) => {
         this.initAudio()
